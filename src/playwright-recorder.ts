b9493b893ea1e947f74bbd91a942ea049ee835d6
Michael Goeke <michael.goeke@dnv.com>
2022-09-07 16:28:08 -0700

    add watching and transpilation of page objects into window context

--- src/playwright-recorder.ts
+++ src/playwright-recorder.ts
@@ -1,6 +1,7 @@
 import { Page, test } from "@playwright/test";
 import * as fs from "fs/promises";
 import * as chokidar from "chokidar";
+import * as ts from "typescript";
 
 export module PlaywrightRecorder {
     //todo: figure out how to decorate .d.ts with default paths
@@ -10,7 +11,7 @@ export module PlaywrightRecorder {
         pageObjectModel: {
             path: './src/page-object-models/',
             filenameConvention: '**/*_page.ts',
-            baseUrl: test.info().config.projects[0].use.baseURL!, //hack: using projects[0] since can't find 'use.*' otherwise
+            baseUrl: <string|undefined>undefined,
             urlToFilePath: (url: string) => url.replace(new RegExp(`^${config.pageObjectModel.baseUrl}`), ''), //strip the baseUrl
             propertySelectorConvention: /(.+)_selector/, //use this to find list of all selectors, and lookup property from selector
             //todo: strip numeric id and guids from url
@@ -23,6 +24,7 @@ export module PlaywrightRecorder {
      */
     export async function startLiveCoding(page: Page, evalScope: (s: string) => any) {
         const isHeadless = test.info().config.projects[0].use.headless; //hack: using projects[0] since can't find 'use.*' otherwise
+        config.pageObjectModel.baseUrl = config.pageObjectModel.baseUrl ?? test.info().config.projects[0].use.baseURL!; //hack: using projects[0] since can't find 'use.*' otherwise
         if (isHeadless !== false) {
             console.error('startLiveCoding called while running headless');
             return;
@@ -65,20 +67,27 @@ export module PlaywrightRecorder {
 
     async function scanAndLoadPageObjectModels(page: Page) {
         const watch = chokidar.watch(`${config.pageObjectModel.path}${config.pageObjectModel.filenameConvention}`);
-        watch.on('add', path => reloadPageObjectModel(page, path)); //todo: filter by lookupPaths
-        watch.on('change', path => reloadPageObjectModel(page, path)); //todo: filter by lookupPaths
+        watch.on('add', path => reloadPageObjectModel(page, path));
+        watch.on('change', path => reloadPageObjectModel(page, path));
         //todo: figure out cleanup of the watcher. Assume current test task being ended is enough.
-
-        const allFiles = watch.getWatched();
-        console.log('todo: iterate and init load all watched page files', { allFiles });
     }
 
     async function reloadPageObjectModel(page: Page, path: string) {
-        const i = import(path);
-        //todo: iterate i to find class in file
-        //reflect across i class, get all properties that match 
+        const fileContents = '' + await fs.readFile(path);
+        const transpiled = ts.transpile(fileContents, { module: ts.ModuleKind.ESNext });
+        //todo: make this work with modules or classes (currently only works with modules)
+        //assume module name is same as filename
+        const className = /\\([^\\]+?).ts/.exec(path)![1]; //extract filename without extension as module name
 
-        await page.addScriptTag({ path });
+        //todo: replace hardcoded string replacements with using typescript lib to walk to AST instead
+        const content = transpiled
+            //export class fixup
+            .replace(`var ${className} = /** @class */ (function () {\r\n    function ${className}() {\r\n    }`, `var ${className} = {};`)
+            .replace(`    return ${className};\r\n}());\r\nexport { ${className} };`, `window.${className} = ${className};`)
+            //export module fixup
+            .replace(`export var ${className};`, `window.${className} = ${className};`)
+
+        await page.addScriptTag({ content });
     }
 
     async function TestingContext_eval(testCallingLocation: { file: string, line: number }, evalScope: (s:string) => any, page: Page, testEval: string, record = true, commandToOverwrite: string | undefined = undefined) {
