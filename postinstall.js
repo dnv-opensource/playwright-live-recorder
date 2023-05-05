0d36d4fede0066fb37c318e559f822c6a863d9fb
Michael Goeke <michael.goeke@dnv.com>
2023-05-05 13:37:58 -0700

    bump the version number (guess I shouldn't have ammeded that last commit)

--- postinstall.js
+++ postinstall.js
@@ -4,5 +4,7 @@ const dest = process.env.INIT_CWD;
 fs.readdirSync('example').forEach(file => {
     try { 
         fs.copyFileSync('example/' + file, dest + '/' + file, fs.constants.COPYFILE_EXCL); 
-    } catch(e) { if (e.code !== 'EEXIST') throw e; }
+    } catch(e) {
+        if (e.code !== 'EEXIST') throw e; //ignore "errors" if file exists, we don't want to overwrite, and we don't need to show an error to the user
+    }
 });
