24d07289e726901296020ec2eb4eb01773733377
Michael Goeke <michael.goeke@dnv.com>
2023-05-05 13:04:46 -0700

    fix postinstall script. remove hacks from hackathon

--- publish-local.bat
+++
@@ -1,4 +0,0 @@
-rd dist /s /q
-call yarn build-fast
-rd /s /q ..\hackathon-2023-playwright-live-recorder\node_modules\@dnvgl\playwright-live-recorder\dist\
-xcopy dist /s ..\hackathon-2023-playwright-live-recorder\node_modules\@dnvgl\playwright-live-recorder\dist\
